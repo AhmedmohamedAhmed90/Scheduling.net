683393eeaefc30770cad754871bb72801c1b8c53
hazem mohamed <hazemm7622@gmail.com>
2024-04-29 17:22:55 +0200

    Login Logic + Login Dto

--- ReactApp1.Server/Controllers/AccountController.cs
+++ ReactApp1.Server/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Client;
 using ReactApp1.Server.Data;
 using ReactApp1.Server.Dtos;
@@ -19,11 +20,36 @@ namespace ReactApp1.Server.Controllers
     {
         private readonly UserManager<Student> visitor;
         private readonly ITokenService _TokenService;
-        public AccountController(UserManager<Student> userManager ,ITokenService TokenService )
+
+        private readonly SignInManager<Student> _signin;
+        public AccountController(UserManager<Student> userManager ,ITokenService TokenService , SignInManager<Student> signin )
         {
             visitor=userManager;
             _TokenService=TokenService;
+            _signin = signin;    
     
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(Login login){
+             if (!ModelState.IsValid){
+                     return BadRequest(ModelState);
+                }
+            var user= await visitor.Users.FirstOrDefaultAsync(x=> x.UserName == login.Username.ToLower());    
+
+            if (user ==null) return Unauthorized("the user not found");
+
+            var result = await  _signin.CheckPasswordSignInAsync(user, login.Password , false);
+
+            if (!result.Succeeded) return Unauthorized("Invalid UserName and / or password");
+
+            return Ok(
+                new NewUser{
+                    UserName=user.UserName,
+                    Email = user.Email,
+                    Token = _TokenService.CreateToken(user)
+                }
+            );
         }
 
        [HttpPost("register")]
@@ -43,7 +69,7 @@ namespace ReactApp1.Server.Controllers
                     Age = registerDto.Age,
                     Year=registerDto.Year,
                     Name=registerDto.Name
-                     
+
                 };
             var createUser = await  visitor.CreateAsync(student , registerDto.Password);
             if(createUser.Succeeded){
---
+++ ReactApp1.Server/Dtos/Login.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReactApp1.Server.Dtos
+{
+    public class Login
+    {
+         [Required]
+        public String? Username {get; set;}
+        [Required]
+        public String? Password { get; set; }
+    }
+}
