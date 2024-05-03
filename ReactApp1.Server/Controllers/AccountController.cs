2beb126741cb2bf5e90d83ad6779a7a240c885af
AhmedmohamedAhmed90 <125593437+AhmedmohamedAhmed90@users.noreply.github.com>
2024-05-03 21:55:31 +0300

    send email in regestration with username and password

--- ReactApp1.Server/Controllers/AccountController.cs
+++ ReactApp1.Server/Controllers/AccountController.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Client;
+using Newtonsoft.Json;
 using ReactApp1.Server.Data;
 using ReactApp1.Server.Dtos;
 using ReactApp1.Server.Interfaces;
@@ -62,7 +64,7 @@ namespace ReactApp1.Server.Controllers
                 
                 var student= new Student
                 {
-                    UserName=registerDto.Username,
+                    // UserName=registerDto.Username,
                     Email=registerDto.Email,
                     Faculty=registerDto.Faculty,
                     Address=registerDto.Address,
@@ -71,8 +73,29 @@ namespace ReactApp1.Server.Controllers
                     Name=registerDto.Name
 
                 };
-            var createUser = await  visitor.CreateAsync(student , registerDto.Password);
+
+                string username = registerDto.Name.Replace(" ", "") + GenerateRandomNumbers(4);
+                student.UserName = username;
+               
+                string password = GenerateRandomNumbers(9)+"Aa#";
+
+            var createUser = await  visitor.CreateAsync(student , password);
             if(createUser.Succeeded){
+               var payload = new
+               {
+                 email = student.Email,
+                 subject = "Your credentials for the University Account",
+                 body = $"Username: {username}\nPassword: {password}"
+               };
+
+            // Use HttpClient to send the request
+             using (var client = new HttpClient())
+             {
+                 var content = new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json");
+                 var response = await client.PostAsync("https://prod-68.westeurope.logic.azure.com:443/workflows/c13c5def438d4022b868c634ed180d89/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=qToVLIyTj0GzNMDn79DNHb0vRsW4QFu_s0KGzVWRDj8", content);
+                 response.EnsureSuccessStatusCode();
+                 Console.WriteLine(await response.Content.ReadAsStringAsync());
+             }
                  var Result = await visitor.AddToRoleAsync(student , "User");
                  if(Result.Succeeded){
                   return Ok(
@@ -95,6 +118,19 @@ namespace ReactApp1.Server.Controllers
          return StatusCode(500 , ex.Message );
         }
     }
+
+[ApiExplorerSettings(IgnoreApi = true)]
+protected string GenerateRandomNumbers(int length)
+        {
+            Random random = new Random();
+            const string chars = "0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+
  }
+
+
 
 }
